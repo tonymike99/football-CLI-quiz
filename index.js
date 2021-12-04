7b76b00f79980f7a688476495185aed87a55546a
Tony Mike Auxen <tonymike99@gmail.com>
2021-12-04 12:53:28 +0530

    code: initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+var readLineSync = require('readline-sync');
+
+console.log(`Welcome to the "Football?" CLI quiz`);
+
+var username = readLineSync.question('Please enter your name: ');
+console.log('Hello ' + username + '! Nice to meet you!\n');
+
+console.log("Let's begin the quiz!\n");
+
+let game = [
+    {
+        question: 'How many teams play in the Premier League? ',
+        answer: '20',
+    },
+    {
+        question: 'Who is the current England manager? ',
+        answer: 'Gareth Southgate',
+    },
+    {
+        question: 'Which country has won the most World Cups? ',
+        answer: 'Brazil',
+    },
+    {
+        question: 'The record number of World Cup goals is 16, scored by who? ',
+        answer: 'Miroslav Klose',
+    },
+    {
+        question: 'Which club has won the most Champions League titles? ',
+        answer: 'Real Madrid',
+    },
+];
+
+function play(game) {
+    let score = 0;
+
+    for (let round of game) {
+        var userAnswer = readLineSync.question(round['question']);
+        if (userAnswer.toUpperCase() === round['answer'].toUpperCase()) {
+            console.log('Correct!');
+            score++;
+        } else console.log('Wrong! The correct answer is ' + round['answer']);
+
+        console.log('Current score: ' + score + '\n');
+    }
+
+    return score;
+}
+
+console.log(
+    'You have answered ' +
+        play(game) +
+        ' out of ' +
+        game.length +
+        ' questions correctly, ' +
+        username
+);
+
+let highScore = [{name: 'Charles', score: 5}];
+
+console.log(
+    "\nCheck out the high scores. If you should be there, ping me and I'll update it"
+);
+for (let item of highScore) {
+    console.log(item.name + ' : ' + item.score);
+}
